68d828b9f0da04d3819de590b8245b7dd209b7ee
Ernest <besonganong_ernest@yahoo.com>
2026-03-30 15:59:18 -0230

    Made changes to app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const app = express();
+const categoryRouter = require("./routes/categories");
+const itemRouter = require("./routes/items");
 
 
 app.set("views", path.join(__dirname, "views"));
@@ -10,11 +12,17 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
+
+app.get("/", (req, res) => res.redirect("/categories"));
+
+app.use("/categories", categoryRouter);
+app.use("/items", itemRouter);
+
 app.use((err, req, res, next) => {
   console.error(err);
   res.status(500).render("error", { message: err.message });
 });
 
 
-const PORT = process.env.PORT || 3000;
+const PORT =  3000;
 app.listen(PORT, () => console.log(`Server running on localhost:${PORT}`));
